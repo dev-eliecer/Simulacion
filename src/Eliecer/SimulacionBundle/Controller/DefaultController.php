ec745a3b28151acdbf870ffd17f3b520fb0b62ba
Eliecer David Pari Alhuay <dev.eliecer@gmail.com>
2014-06-12 05:47:03 -0500

    Agregando Bundle simulacion

---
+++ src/Eliecer/SimulacionBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Eliecer\SimulacionBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('EliecerSimulacionBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Eliecer/SimulacionBundle/EliecerSimulacionBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Eliecer\SimulacionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EliecerSimulacionBundle extends Bundle
+{
+}
